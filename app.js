223d9abf70b6f683690327575edc10c18b7e03d6
tabarak5 <tacitim5@gmail.com>
2021-01-07 20:23:56 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-const arry=[
-    {namee:"aasim",age:24,class:"BSIT"},
-    {namee:"Maheen",age:23,class:"BSCS"},
-]
-// console.log(arry.map(user=>{{user.namee}}))
-document.write(arry.map(user=>{<li>{user.name}</li>}));
+// const arry=[
+//     {namee:"aasim",age:24,class:"BSIT"},
+//     {namee:"Maheen",age:23,class:"BSCS"},
+// ]
+// // console.log(arry.map(user=>{{user.namee}}))
+// document.write(arry.map(user=>{<li>{user.name}</li>}));
+// let namee="aasim";
+// let caste ="soomro";
+// let designation="CS Student"
+// let sentence=`${namee} ${caste} is ${designation}`;
+// console.log(sentence);
+// console.log(arry.map(arr=>{arr.age}))
+let aasim=[ "Muhammad Aasim","Soomro",24];
+
+let [Name,Caste,Age]=aasim;
+
