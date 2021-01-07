ab377c6cc8fd323418185ea70fc16db7326e0b67
tabarak5 <tacitim5@gmail.com>
2021-01-07 14:26:09 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const arry=[
     {namee:"aasim",age:24,class:"BSIT"},
-    {namee:"Maheen Crush",age:23,class:"BSCS"},
+    {namee:"Maheen",age:23,class:"BSCS"},
 ]
-console.log(arry.map(user=>{{user.namee}}))
-// document.write(arry.map(user=>{<li>{user.name}</li>}));
+// console.log(arry.map(user=>{{user.namee}}))
+document.write(arry.map(user=>{<li>{user.name}</li>}));
